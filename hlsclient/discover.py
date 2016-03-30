9ea26d5b0275ed2268dd6caf577aed5661eb8650
feng liu <feng.liu@vhall.com>
2016-03-30 10:51:57 +0800

    add playlists.json and update discovery.py code

--- hlsclient/discover.py
+++ hlsclient/discover.py
@@ -1,15 +1,18 @@
 import json
 import urllib2
 import urlparse
+import os
 from collections import namedtuple
 
 Server = namedtuple('Server', 'server port')
 
+
 def get_servers(playlists):
     return dict([(playlist_name, playlist_data['servers']) for
-        playlist_name, playlist_data in playlists['streams'].items()])
+                 playlist_name, playlist_data in playlists['streams'].items()])
 
-def discover_playlists(config):
+
+def discover_playlists():
     '''
     Returns a dictionary of streams to be consumed with the following format:
     {
@@ -23,20 +26,38 @@ def discover_playlists(config):
         }
     }
     '''
-    api_url = config.get('discover', 'api_url')
-    playlists = _get_streams_from_url(api_url)
+    # api_url = config.get('discover', 'api_url')
+    # playlists = _get_streams_from_url(api_url)
+    playlists = None
+    playlists_path = os.path.join(os.path.dirname(__file__),
+                                  r'../playlists.json')
+    with open(playlists_path, 'r') as f:
+        playlists = json.load(f)
+    if playlists is None:
+        return None
     for playlist, data in playlists['streams'].items():
         data['servers'] = map(_url_to_server, data['servers'])
+    print playlists
     return playlists
+
 
 def _get_streams_from_url(url):
     # FIXME: implement error checking
-    timeout = 30 # FIXME: too long & magic number
+    timeout = 30  # FIXME: too long & magic number
     return json.load(urllib2.urlopen(url, timeout=timeout))
+
 
 def _url_to_server(server):
     parsed_url = urlparse.urlparse(server)
-    server_url = '{scheme}://{hostname}'.format(scheme=parsed_url.scheme, hostname=parsed_url.hostname)
+    server_url = '{scheme}://{hostname}'.format(
+        scheme=parsed_url.scheme, hostname=parsed_url.hostname)
     default_port = 443 if parsed_url.scheme == 'https' else 80
     port = parsed_url.port or default_port
     return Server(server=server_url, port=port)
+
+if __name__ == '__main__':
+    # url = "http://cchlslivepc03.e.vhall.com/vhall/1207862104/index.m3u8"
+    # s = _url_to_server(url)
+    # print s.server
+    # print s.port
+    discover_playlists()
